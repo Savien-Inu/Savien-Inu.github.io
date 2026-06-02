2c983c2cd4befdba067cdad9ca9aa35796ca6f45
Savien-Inu <85548285+Savien-Inu@users.noreply.github.com>
2026-06-02 10:58:29 +0800

    post2

---
+++ blog-posts/post-2.js
@@ -0,0 +1,11 @@
+const BLOG_POST = {
+      id: 1,
+      title: "Another post!",
+      content: `I've modified the blog post system again!
+
+Please ignore, will delete this later! This is just a test!
+> 
+*Until we meet again~*`,
+      author: "Savi",
+      date: "June 2, 2026"
+};
