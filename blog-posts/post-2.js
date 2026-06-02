82df62c832d9c0679cd91b47b4c709563e0b87d5
Savien-Inu <85548285+Savien-Inu@users.noreply.github.com>
2026-06-02 21:28:29 +0800

    blogpost 2

--- blog-posts/post-2.js
+++ blog-posts/post-2.js
@@ -1,9 +1,9 @@
 const BLOG_POST = {
       id: 2,
-      title: "Another post!",
-      content: `I've modified the blog post system again!
-
-Please ignore, will delete this later! This is just a test!
+      title: "Image Test!",
+      content: `Just testing the image feature.
+      I present: Kino!
+      ![have you ever felt jealous of birds?](https://64.media.tumblr.com/b1c0a09eeb3ea871a1ed280b9a608da8/tumblr_inline_p2x0jfvUTz1sv288m_500.gif)
 > 
 *Until we meet again~*`,
       author: "Savi",
