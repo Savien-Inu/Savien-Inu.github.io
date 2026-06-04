63364092db27fb294eeca48a3d91da519663d050
Savien-Inu <85548285+Savien-Inu@users.noreply.github.com>
2026-06-05 00:14:00 +0800

    Ver 1.03

--- blog-posts/post-2.js
+++ blog-posts/post-2.js
@@ -1,10 +1,10 @@
 const BLOG_POST = {
       id: 2,
       title: "Image Test!",
-      content: `Just testing the image feature.
-      I present: Kino!
-      ![have you ever felt jealous of birds?](https://64.media.tumblr.com/b1c0a09eeb3ea871a1ed280b9a608da8/tumblr_inline_p2x0jfvUTz1sv288m_500.gif)
-> 
+      content: `
+Just testing the image feature.
+I present: Kino!
+![have you ever felt jealous of birds?](https://64.media.tumblr.com/b1c0a09eeb3ea871a1ed280b9a608da8/tumblr_inline_p2x0jfvUTz1sv288m_500.gif)
 *Until we meet again~*`,
       author: "Savi",
       date: "June 2, 2026"
