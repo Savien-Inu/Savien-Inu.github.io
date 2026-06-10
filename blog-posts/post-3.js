80f5963ad6816938192ffda4a3881840a76ba212
Savien-Inu <85548285+Savien-Inu@users.noreply.github.com>
2026-06-10 22:45:26 +0800

    ver 1.07

---
+++ blog-posts/post-3.js
@@ -0,0 +1,28 @@
+const BLOG_POST = {
+      id: 2,
+      title: "Birds",
+      content: `
+<img src="https://64.media.tumblr.com/b1c0a09eeb3ea871a1ed280b9a608da8/tumblr_inline_p2x0jfvUTz1sv288m_500.gif" alt="Have you ever felt jealous of birds?" class="about-image" loading="lazy">
+<blockquote> Have you ever felt jealous of birds? </blockquote>
+<p>Birds are often used to represent freedom. Being able to fly wherever, whenever.</p>
+<p>To be able to explore the world like that, doesn't it sound amazing? It's no wonder why flight has been one of man's first dreams since the beginning of time.</p>
+<p>Kino's Journey often includes birds in their stories. The first Kino stating the famous quote:</p>
+<blockquote> Whenever people see birds flying through the sky, it's said that they get the urge to go on a journey. </blockquote>
+<p>The intro of both the 2003 and 2017 versions of the anime have birds flying alongside Kino as they travel the lands with Hermes.</p>
+<p>There was a story I was writing titled "S-1229", it was supposed to tell the story of an a young adult learning what it means to be "free" after spending so much of their life under the eyes of their strict caretakers the "Arbiters". The story opened with the line:</p>
+<blockquote> I've always wondered what it was like to be a bird. </blockquote>
+<img src="https://imgur.com/kqHWlUe.png" alt="Have you ever felt jealous of birds?" width="168" height="242" style="vertical-align:middle">
+<p>This drawing of a bird accompanied the opening line. It was supposed to represent the MC's sketch of a bird from what they've seen in books. I had drawn this nearly 3 years ago when I had just started learning how to draw digitally with a pen tablet.</p>
+<p>At the time of writing that story, I was having my own troubles with my "freedom". I was nearing my graduation for high school and preparing for college but... I didn't know what course to take. Well, I did have a course in mind but my parents, moreso my father, promptly refused that choice. So I chose a course I thought was the closest to it. I didn't like the idea that my own future was being treaded by my parents. It's true, they did have more experience than me and only had good intentions, but all of it still felt wrong.</p>
+<p>I wanted to include that personal element in the story; the sorrow of not being able to choose for yourself.</p>
+<p>I had stopped writing up until the point where the MC is unexpectedly released from the watchful eyes of the "Arbiters". I was quite proud of what I had written so I shared it with friends and family. 
+
+Of course, since it was just the prologue -- they weren't that interested in the story. I never did continue writing it. My troubles with "freedom" aren't as awful as then but the question of whether or not I should continue my studies under this course still linger. Funnily enough, those feelings seem to emerge whenever I spot birds flying past.</p>
+<p>I initially just wanted to test the image feature for my blog posts but ended up going on a tangent about birds and freedom so I decided to make this the first formal blog post on my site! This is also my first time writing a blog post online! Probably the most I've ever shared about myself publicly.</p>
+<p>If you made it this far, thank you so much for taking the time to read the ramblings of this wanderer. I wish I could give something in return but what I can offer is quite limited in this digital space. Instead, have one of my favorite arrangements of <strong>Touhou 11's "Satori Maiden ~ 3rd Eye."</strong> I hope this lightens the load of whatever troubles you're carrying right now.</p>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/XBhZK9nxdtQ?si=CC7ecENTpw9ZcESk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<div class="signature">Until we meet again~</div>
+`,
+      author: "Savi",
+      date: "June 6, 2026"
+};
